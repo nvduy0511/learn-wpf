681f56d293c98a8e722f4a8471a2d124458bb3df
nvduy0511 <nvduy.k19hutech@gmail.com>
2022-10-19 15:35:46 +0700

    done tree-view

--- learn-wpf/MainWindow.xaml.cs
+++ learn-wpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,78 @@ namespace learn_wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                TreeViewItem item = new TreeViewItem()
+                {
+                    Header = drive,
+                    Tag = drive
+                };
+
+                item.Items.Add(null);
+
+                item.Expanded += Folder_Expanded;
+                tvFolder.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            item.Items.Clear();
+
+            string fullPath = item.Tag.ToString();
+
+            List<String> subFolder = new List<string>();
+
+            try
+            {
+                var dir = Directory.GetDirectories(fullPath);
+                if (dir.Length > 0)
+                    subFolder.AddRange(dir);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            subFolder.ForEach(folder =>
+            {
+                TreeViewItem subItem = new TreeViewItem()
+                {
+                    Header = getFolderNameFromPath(folder),
+                    Tag = folder
+                };
+
+                subItem.Expanded += Folder_Expanded;
+
+                subItem.Items.Add(null);
+                item.Items.Add(subItem);
+
+            });
+
+        }
+
+        private string getFolderNameFromPath(string path)
+        {
+
+            if(string.IsNullOrEmpty(path))
+                return string.Empty;
+            path = path.Replace('/', '\\');
+            int index = path.LastIndexOf("\\");
+            if (index < 0)
+                return path;
+            return path.Substring(index+1);
+
         }
     }
 }
